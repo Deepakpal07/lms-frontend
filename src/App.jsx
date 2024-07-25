9fc2c27cd243df9f2b29e6aa8d6b436f86983ea5
Deepakpal07 <paldeepak3341@gmail.com.live>
2024-07-25 14:01:20 +0530

    Prepared course details component

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Login from './Pages/Login';
 import CourseList from './Pages/Course/CourseList';
 import Contact from './Pages/Contact';
 import Denied from './Pages/Denied';
+import CourseDescription from './Pages/Course/CourseDescription';
 
 function App() {
   
@@ -17,12 +18,14 @@ function App() {
     <>
       <Routes>
          <Route path="/" element={<HomePage />} ></Route>
-         <Route path="/about" element={<AboutUs/>}></Route>
-         <Route path="/signup" element={<Signup/>}></Route>
-         <Route path="/login" element={<Login/>}></Route>
+         <Route path="/about" element={<AboutUs/>}></Route> 
          <Route path="/courses" element={<CourseList/>}></Route>
          <Route path="/contact" element={<Contact/>}></Route>
          <Route path="/denied" element={<Denied/>}></Route>
+         <Route path="/course/description" element={<CourseDescription/>}></Route>
+
+         <Route path="/signup" element={<Signup/>}></Route>
+         <Route path="/login" element={<Login/>}></Route>
 
 
          <Route path="*" element={<NotFound/>}></Route>
